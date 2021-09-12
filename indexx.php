59ca98c130414b188f2d79e1efb385527c08bbd7
rohitredekar <rohitredekar89@gmail.com>
2021-09-12 19:52:29 +0530

    commit

---
+++ indexx.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>design page</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+	<link rel="stylesheet"  href="style.css">
+</head>
+<body>
+		<nav class="nav">
+			<ul class="nav-ul" id="myTopnav">
+			<li class="nav-ul1">
+			<li class="nav-ul-li">
+				<img class="nav-images" src="cf/cf/cflogo.png" width="200" height="30px"></li></li>
+				<li class="nav-ul2">
+				<li class="nav-ul-li"><a  href="#"><p class="active">HOME</p></a></li>
+				<li class="nav-ul-li"><a href="#">Activities</a></li>
+				<li class="nav-ul-li"><a href="#">Messages</a></li></li>
+			<li class="nav-ul3">
+			<li class="nav-ul-li"><img class="nav-images2" src="cf/cf/img4.jpg" width="50px" height="50px"></li>
+			<li class="nav-ul-li" style="color:white">Stella John</li>
+			<li class="nav-ul-li"><a href="#">Logout</a></li></li></ul>
+		</nav>
+
+	<div class="grid-container">
+		<div class="item1">
+				<img class="img-grid-one" src="cf/cf/img4.jpg">
+				<ul class="img-grid-one-ul">
+					<li><b>Stella John</b></li>
+					<li><button type="button" style="background-color: #00BFF3; color: white">EDIT PROFILE</button></li>
+				</ul>
+					<p style="text-align: left;">Short Bio:</p>
+					<p style="text-align: left;">Lorem Ipsum is simply dummy text of the printing typesetting industry are and typesetting  typesetting industry  and typesetting industry has been the</p><br>
+		<div>	
+				<table>
+				<tbody class="img-grid-one-table">
+					 <tr>
+						<td><i class="bi bi-geo-alt"></i></td>
+						<td><li class="light">7689 6557 5654</li>
+						<li class="light">simply dummy text</li></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td><i class="bi bi-stopwatch"></i></td>
+						<td><li class="light">Member Since</li>
+						<li class="light">November 2021</li></td>
+					</tr>
+				</tbody>
+				</table>
+		</div><br><br>
+		<div class="img-grid-one-table2">
+			<table>
+				<tr class="img-grid-one-tr">
+					<td>FOLLOWERS (34)<i id="caret" class="bi bi-caret-right-fill"></i></td>
+				</tr>
+				<tr class="img-grid-one-tr">
+					<td>FOLLOWING (44)<i id="caret" class="bi bi-caret-right-fill"></i></td>
+				</tr>
+				<tr class="img-grid-one-tr">
+					<td>TROPHES <span>(show all)</span> (8)<i id="caret" class="bi bi-caret-right-fill"></i></td>
+				</tr>
+				<tr class="img-grid-one-tr">
+					<td >SUBSCRIBE ACTIVITIES (34)<span><i id="caret" class="bi bi-caret-down-fill"></i></span></td>
+				</tr>
+			</table>
+		</div>
+		<div class="submenu">
+			<p>RUNNING</p>
+			<p>WEIGHT LIFTING</p>
+			<p>BASKET BALL</p>
+			<p>SOFTBALL</p>
+			<p>BASEBALL</p>
+			<p>TENNIES</p>
+		</div>
+	</div>
+
+  <div class="item2">
+  	<div class="img-grid-two">
+  		<table>
+  			<tbody>
+  				<tr>
+  					<td><img class="img-grid-two-img" src="cf/cf/img5.jpg"></td>
+  					<td><div class="time">Today, 3.39pm</div>
+  					<p style="text-align: left; font-size: 20px;"><b>Poul Brunet</b> Posted in <span>Running</span><p>
+  					<p style="text-align: left;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500</p></td>
+  				</tr>
+  			</tbody>
+  		</table>
+  		<p style="text-align: right;"><span>Read more.></span></p>
+  		<hr>
+  		<table  class="espace">
+  			<tbody>
+  				<tr>
+  					<td><img class="img-grid-two-img" src="cf/cf/img3.jpg"></td>
+  					<td>
+  						<p style="text-align: left; font-size: 20px; color: #434A54;"><b>Anney Hathway</b></p>
+  					<p style="text-align: left;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p></td>
+  				</tr>
+  				<br><br>
+  				<tr>
+  					<td><img class="img-grid-two-img" src="cf/cf/img6.jpg"></td><br>
+  					<td>
+  						<p style="text-align: left; font-size: 20px; color: #434A54;"><b>Remy Daniel</b></p>
+  					<p style="text-align: left;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p></td>
+  				</tr>
+  			</tbody>
+  		</table>
+  	</div>
+  	<p style="text-align: right;"><span>More comments.></span></p>
+  	<div class="img-grid-two-post"><br>
+  		Comments <input type="search>" size="40px" placeholder="write your comments here...">
+  		<button type="button" style="background-color: #00BFF3; color:white">Post</button>
+  	</div>
+  	<div class="img-grid-two">
+  		<table>
+  			<tbody>
+  				<tr>
+  					<td><img class="img-grid-two-img" src="cf/cf/img5.jpg"></td>
+  					<td>
+  						<div class="time">yesterday, 6.49pm</div>
+  					<p style="text-align: left; font-size: 20px;"><b>Poul Brunet</b> Posted in<span>Baseball<span></p>
+  					<p style="text-align: left;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</p></td>
+  				</tr>
+  			</tbody>
+  		</table>
+  		<p style="text-align: right;"><span>Read more.></span></p>
+  		<hr>
+  		<table  class="espace">
+  			<tbody>
+  				<tr>
+  					<td><img class="img-grid-two-img" src="cf/cf/img3.jpg"></td>
+  					<td><br>
+  					<p style="text-align: left; font-size: 20px; color: #434A54;"><b>Anney Hathway</b></p>
+  				<p style="text-align: left;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy </p></td>
+  				</tr>
+  				<br>
+  				<tr>
+  					<td><img class="img-grid-two-img" src="cf/cf/img6.jpg"></td>
+  					<td>
+  				<p style="text-align: left; font-size: 20px; color: #434A54;"><b>Remy Daniel</b></p>
+  				<p style="text-align: left;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p></td>
+  				</tr>
+  			</tbody>
+  		</table>
+  	</div>
+  	<p style="text-align: right;"><span>More comments.></span></p>
+  	<div class="img-grid-two-post"><br>
+  		Comments <input type="search" size="40" placeholder="write your comments here...">
+  		<button type="button" style="background-color: #00BFF3; color:white">Post</button>
+  	</div>
+
+  </div>
+
+
+
+  <div class="item3">
+  	<div class="img-grid-three">
+		  	<p style="text-align: left; font-size: 20px; color: #434A54;"><b>Notification</b></p>
+
+		  	<p style="text-align: left;"><span>John</span> commented <b> it was really helful and..</b>on your post <span>'5 way to successfully...'</span> in <span>Running</span></p>
+		  	<p  class="light" style="text-align: right;">15 Min Before</p>
+		  	<hr>
+		  		<p style="text-align: left;"><span>John</span> commented <b> it was really helful and..</b>on your post <span>'5 way to successfully...'</span> in <span>Running</span></p>
+		  		<p  class="light" style="text-align: right;">1 Hour Before</p>
+		  
+		  	<hr>
+		  		<p style="text-align: left;"><span>John</span> commented <b> it was really helful and..</b>on your post <span>'5 way to successfully...'</span> in <span>Running</span></p>
+		  		<p  class="light" style="text-align: right;">yesterday</p>
+		  
+		  	<hr>
+		  		<p style="text-align: left;"><span>John</span> commented <b> it was really helful and..</b>on your post <span>'5 way to successfully...'</span> in <span>Running</span></p>
+		  		<p  class="light" style="text-align: right;">2 Days Before</p>
+  
+  			<hr>
+  		</div>
+  </div> 
+  <hr> 
+  <div class="item4">
+  	<div class="img-grid-four">
+			<p style="text-align: left; font-size: 20px; color: #434A54;"><b>Recent Meassages</b></p>
+		<div>
+			<p style="text-align: left; font-size: 20px;"><span>Remy daniel</span></p>
+			<p style="text-align: left;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+			<button type="button" class="rply-btn">Reply</button>
+  </div>
+  <br><br>
+  <hr>
+   <div>
+  		<p style="text-align: left; font-size: 20px;"><span>Poul Brounet</span></p>
+  		<p style="text-align: left;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+  		<button type="button" class="rply-btn">Reply</button>
+  	</div>
+  	<br><br>
+  	<hr>
+   </div>
+ </div>
+</div>
+ <br><br><br><br><br><br><br><br>
+<footer class="footer"></footer><div class="subfooter"> copyRight 2014 All Right Reserved</div>
+</body>
+</html>
